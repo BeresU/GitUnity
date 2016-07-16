fede95f06e767069c3f664541f5bf606a02eaeda
BeresU <udiberes@gmail.com>
2016-07-16 12:40:22 +0300

    Created another new Script
    Created another new Script

---
+++ GitUnity_01/Assets/Scripts/SomeScript02.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeScript02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
